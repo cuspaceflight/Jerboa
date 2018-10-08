1a729754f9f4b68d8c25fb60351e1a5b62143bd6
Sean McGuire <sean.mcguire415@gmail.com>
2018-10-08 16:49:17 +0100

    added gps code

--- fw/main.c
+++ fw/main.c
@@ -1,11 +1,20 @@
 #include "ch.h"
 #include "hal.h"
 
+#include "gps.h"
+
+
 int main(void)
 {
 	/* Initialise ChibiOS */
 	halInit();
 	chSysInit();
+
+    /* Configure GPS to Produce 1MHz Reference */
+    gps_init(&SD1, true, false, true);
+
+    /* Start GPS State Machine */
+    gps_thd_init();
 
 	/* Main Loop */
     while (true) {
